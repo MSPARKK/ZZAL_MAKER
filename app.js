4067f64188faa1f43ede54d0ed419e478279ac95
MSPARKK <pms93009@gmail.com>
2023-05-22 22:05:23 +0900

    삭제 버튼을 추가

--- app.js
+++ app.js
@@ -31,19 +31,24 @@ canvas.addEventListener('mousedown', function (e) {
 
     // Check each text object to see if we've clicked inside
     texts.forEach(function (textObj, index) {
-        if (mouseX > textObj.x - dragPadding && mouseX < textObj.x + textObj.width + dragPadding &&
-            mouseY > textObj.y - textObj.height - dragPadding && mouseY < textObj.y + dragPadding) {
-            textObj.isDragging = true;
-            textObj.offsetX = mouseX - textObj.x;
-            textObj.offsetY = mouseY - textObj.y;
 
-            // Move the selected text to the end of the array
-            texts.splice(index, 1);
-            texts.push(textObj);
+        if (mouseX > textObj.buttonX && mouseX < textObj.buttonX + textObj.buttonSize &&
+            mouseY > textObj.buttonY && mouseY < textObj.buttonY + textObj.buttonSize) {
+            textObj.isButtonClicked = true;
+        } else
+            if (mouseX > textObj.x - dragPadding && mouseX < textObj.x + textObj.width + dragPadding &&
+                mouseY > textObj.y - textObj.height - dragPadding && mouseY < textObj.y + dragPadding) {
+                textObj.isDragging = true;
+                textObj.offsetX = mouseX - textObj.x;
+                textObj.offsetY = mouseY - textObj.y;
 
-            // Update the selected text when starting to drag
-            selectedText = textObj;
-        }
+                // Move the selected text to the end of the array
+                texts.splice(index, 1);
+                texts.push(textObj);
+
+                // Update the selected text when starting to drag
+                selectedText = textObj;
+            }
     });
 });
 
@@ -53,6 +58,10 @@ function drawText(textObj) {
         ctx.fillStyle = "#FFEEAA";
         ctx.fillRect(textObj.x - dragPadding, textObj.y - textObj.height - dragPadding,
             textObj.width + (2 * dragPadding), textObj.height + (2 * dragPadding));
+        // Draw the button
+        ctx.fillStyle = "#FF0000"; // Set the button color to red
+        ctx.fillRect(textObj.buttonX, textObj.buttonY, textObj.buttonSize, textObj.buttonSize);
+
     }
 
     ctx.fillStyle = "black";
@@ -60,20 +69,41 @@ function drawText(textObj) {
 }
 
 canvas.addEventListener('mouseup', function (e) {
-    texts.forEach(function (textObj) {
-        textObj.isDragging = false;
+    texts.forEach(function (textObj, index) {
+        if (textObj.isButtonClicked) {
+            // Remove the text from the array
+            texts.splice(index, 1);
+
+            // If this was the selected text, clear the selection
+            if (textObj === selectedText) {
+                selectedText = null;
+            }
+        } else {
+            textObj.isDragging = false;
+        }
+
+        // Clear the button clicked state
+        textObj.isButtonClicked = false;
     });
 
-    // Optional: You can clear the selection when releasing the mouse
-    // selectedText = null;
+    // Redraw everything
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    texts.forEach(drawText);
 });
 
 // // mousemove event
 canvas.addEventListener('mousemove', function (e) {
     texts.forEach(function (textObj) {
         if (textObj.isDragging) {
+            // Calculate the new button position based on the new text position
+            var newButtonX = e.clientX - canvas.offsetLeft - textObj.offsetX + textObj.width + dragPadding;
+            var newButtonY = e.clientY - canvas.offsetTop - textObj.offsetY - textObj.height - dragPadding;
+
+            // Update the text and button position
             textObj.x = e.clientX - canvas.offsetLeft - textObj.offsetX;
             textObj.y = e.clientY - canvas.offsetTop - textObj.offsetY;
+            textObj.buttonX = newButtonX;
+            textObj.buttonY = newButtonY;
         }
     });
     // Redraw everything
@@ -92,6 +122,10 @@ addButton.addEventListener('click', function () {
     var inputText = inputField.value; // Get the text from the input field
 
     if (inputText) { // Only add the text if the input is not empty
+
+        // Add a button size to the text object
+        var buttonSize = 20;
+
         var newText = {
             text: inputText,
             // x: 50,
@@ -102,7 +136,11 @@ addButton.addEventListener('click', function () {
             height: measureText(inputText, "20px Arial").height,
             isDragging: false,
             offsetX: 0,
-            offsetY: 0
+            offsetY: 0,
+            buttonX: (canvas.width / 2 - measureText(inputText, "20px Arial").width / 2) + measureText(inputText, "20px Arial").width + dragPadding,
+            buttonY: (canvas.height / 2 + measureText(inputText, "20px Arial").height / 2) - measureText(inputText, "20px Arial").height - dragPadding,
+            buttonSize: buttonSize,
+            isButtonClicked: false
         };
 
         // If there was a previously selected text, deselect it
